819e692a07ef93e8715000d8aeee144488181185
DAVID TARREGA <u1979273@campus.udg.edu>
2024-03-05 11:13:51 +0100

    los html han sido cambiados a jquery, ahora faltan alguns js cambiarlos a jquery

--- js/game.js
+++ js/game.js
@@ -1,14 +1,16 @@
 import { game as gController } from "./memory.js";
 
-var game = $('#game');
+$(document).ready(function() {
+    var game = $('#game');
 
-gController.init(updateSRC).forEach(function(card, indx){
-    game.append('<img id="c'+indx+'" class="card" title="card">');
-    card.pointer = $('#c'+indx);
-    card.pointer.on('click', () => gController.click(card));
-    card.pointer.attr("src", card.current);
+    gController.init(updateSRC).forEach(function(card, indx){
+        game.append('<img id="c'+indx+'" class="card" title="card">');
+        card.pointer = $('#c'+indx);
+        card.pointer.on('click', () => gController.click(card));
+        card.pointer.attr("src", card.current);
+    });
+
+    function updateSRC(){
+        this.pointer.attr("src", this.current);
+    }
 });
-
-function updateSRC(){
-    this.pointer.attr("src", this.current);
-}
--- js/options.js
+++ js/options.js
@@ -1,17 +1,23 @@
 var options = function(){
     const default_options = {
-        pairs:2,
-        difficulty:'normal'
+        pairs: 2,
+        difficulty: 'normal'
     };
     
     var pairs = $('#pairs');
     var difficulty = $('#dif');
 
-    var options = JSON.parse(localStorage.options||JSON.stringify(default_options));
+    var options = JSON.parse(localStorage.options || JSON.stringify(default_options));
     pairs.val(options.pairs);
     difficulty.val(options.difficulty);
-    pairs.on('change',()=>options.pairs = pairs.val());
-    difficulty.on('change',()=>options.difficulty = difficulty.val());
+    
+    pairs.on('change', function() {
+        options.pairs = pairs.val();
+    });
+
+    difficulty.on('change', function() {
+        options.difficulty = difficulty.val();
+    });
 
     return { 
         applyChanges: function(){
@@ -26,11 +32,11 @@ var options = function(){
     }
 }();
 
-$('#default').on('click',function(){
+$('#default').on('click', function(){
     options.defaultValues();
 });
 
-$('#apply').on('click',function(){
+$('#apply').on('click', function(){
     options.applyChanges();
     location.assign("../");
 });
